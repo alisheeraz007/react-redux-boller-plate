f5eaf3d472c5383a4a2e6a800edd70981752dbfc
alisheeraz007 <alisheeraz007@gmail.com>
2019-09-16 17:47:55 +0500

    boller plate

---
+++ src/config/configKey.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyDzfSrPC2lSzOhJtxozRtYNgddYX_nHyRs",
+  authDomain: "auctionhere.firebaseapp.com",
+  databaseURL: "https://auctionhere.firebaseio.com",
+  projectId: "auctionhere",
+  storageBucket: "",
+  messagingSenderId: "743553995247",
+  appId: "1:743553995247:web:a1fc9b0eef645e6a10a61a"
+};
+
+  export default config;
